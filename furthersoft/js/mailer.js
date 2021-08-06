2fd173ecc81a3f0bb8bc395427b29f41f97699e7
kicirsami06 <kicirsami06@gmail.com>
2021-08-06 12:46:39 -0400

    update

---
+++ furthersoft/js/mailer.js
@@ -0,0 +1,33 @@
+var express = require('express'),
+    nodemailer = require("nodemailer");
+
+app = express.createServer();
+
+app.use(express.bodyParser());
+
+app.post('/formProcess', function(req, res) {
+    var data = req.body;
+
+    var smtpTransport = nodemailer.createTransport("SMTP", {
+        service: "Hotmail",
+        auth: {
+            user: "email@gmail.com",
+            pass: "gmailPassword"
+        }
+    });
+
+    smtpTransport.sendMail({ //email options
+        from: "Sender Name <email@gmail.com>",
+        to: "Receiver Name <receiver@email.com>", // receiver
+        subject: "Emailing with nodemailer", // subject
+        html: data // body (var data which we've declared)
+    }, function(error, response) { //callback
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("Message sent: " + res.message);
+        }
+
+        smtpTransport.close();
+    });
+});
